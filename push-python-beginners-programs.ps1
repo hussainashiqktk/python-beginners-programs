0f9fa2066390ad73f50cfe1d57f220d24eb2524b
HUSSAIN ASHIQ <hussainashiqkhattak@gmail.com>
2023-01-19 11:10:16 +0500

    Commit message

---
+++ push-python-beginners-programs.ps1
@@ -0,0 +1,17 @@
+
+
+# Set the local and remote repository paths
+$localRepo = "G:\My GitHub Repos\python-beginners-program"
+$remoteRepo = "https://github.com/hussainashiqktk/python-beginners-programs"
+
+# Change to the local repository directory
+cd $localRepo
+
+# Add all changes to the repository
+git add .
+
+# Commit the changes with a message
+git commit -m "Commit message"
+
+# Push the changes to the remote repository
+git push origin main
